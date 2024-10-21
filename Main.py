51dbf3eb0eb43c5be81e67b6d7636f001c040a6f
doaakorkar <doaa.korkar28@gmail.com>
2024-10-21 06:01:01 +0300

    new

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 FastaFile= "exampledata/seqmultiline.fasta"
 
+class Sequence():
+    def __init__(self, fileName):
+        self.fileName= fileName
 
-
-from SeqMod.seqIO import Read_Fasta, get_sequence_lenght
-mySeqs= Read_Fasta(FastaFile)
-seqLenght=get_sequence_lenght(mySeqs)
-print(seqLenght)
+mySeqs=Sequence(FastaFile)
+print(mySeqs)
+# from SeqMod.seqIO import Read_Fasta, get_sequence_lenght
+# mySeqs= Read_Fasta(FastaFile)
+# seqLenght=get_sequence_lenght(mySeqs)
+# print(seqLenght)
 #print(mySeqs)
