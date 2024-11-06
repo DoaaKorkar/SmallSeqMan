2d8f38f11d658f435c5e2b15784533a655718bf6
doaakorkar <doaa.korkar28@gmail.com>
2024-11-06 10:10:12 +0200

    new

--- Main.py
+++ Main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # from SeqMod.seqIO import Read_Fasta, get_sequence_lenght
-from SeqClass.Classes import Sequence
-
+from SeqClass.sequences import Sequence
+from SeqClass.DNA import DNA
 FastaFile= "exampledata/seqmultiline.fasta"
 
 
 
 mySeqs=Sequence(FastaFile)
 
-print(mySeqs.get_seqs())
-print(mySeqs.get_seqs_length())
-print(mySeqs.get_GC_content())
-print(mySeqs.get_AT_content())
+# print(mySeqs.get_seqs())
+# print(mySeqs.get_seqs_length())
+# print(mySeqs.get_GC_content())
+# print(mySeqs.get_AT_content())
 # print(mySeqs.get_Reverse())
 # print(mySeqs.get_Complement())
-
+print(DNA.get_reverse_complement(mySeqs))
 ###print(mySeqs.get_seq_info())
 # print(mySeqs)
 # print(type(mySeqs))
---
+++ SeqClass/DNA.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from SeqClass.sequences import Sequence
+from SeqMod.seqIO import *
+class DNA(Sequence):
+    def get_reverse_complement(self):
+        if "seqs" not in dir(self):
+            self.get_seqs
+        # self.reve= Reverse(self.Seqs)
+        # self.reve_comp= complement(self.reve)
+        # return self.reve_comp
+        return Reverse_Complement(self.get_seqs)
+
+    
